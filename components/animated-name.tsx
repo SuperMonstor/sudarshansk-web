50201c78511659e632dfcf362f5020bc8b9a1966
Sudarshan S <senthilsudarshan@gmail.com>
2025-04-10 02:09:18 +0530

    changes

--- components/animated-name.tsx
+++ components/animated-name.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link';
-import { unstable_ViewTransition as ViewTransition } from 'react';
+import Link from "next/link"
+import { unstable_ViewTransition as ViewTransition } from "react"
 
 export function AnimatedName() {
-  return (
-    <ViewTransition>
-      <Link
-        href="/"
-        className="flex mb-8 font-medium text-gray-400 dark:text-zinc-400"
-      >
-        Lee Robinson
-      </Link>
-    </ViewTransition>
-  );
+	return (
+		<ViewTransition>
+			<Link
+				href="/"
+				className="flex mb-8 font-medium text-gray-400 dark:text-zinc-400"
+			>
+				Sudarshan S
+			</Link>
+		</ViewTransition>
+	)
 }
